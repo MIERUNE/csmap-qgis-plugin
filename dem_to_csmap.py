df6ae8aa1cf622551b911b14b1cf3bb6cbae351d
KeiTa4446 <geo.jagaimo@gmail.com>
2024-06-06 16:43:43 +0900

    .tif以外の拡張子の入力に対応，output_pathが競合していた部分の修正

--- dem_to_csmap.py
+++ dem_to_csmap.py
@@ -3,6 +3,7 @@ import os
 from PyQt5.QtWidgets import QDialog
 from qgis.PyQt import uic
 from qgis.utils import iface
+from qgis.core import QgsRasterLayer
 
 from csmap_py.csmap import process
 
@@ -16,28 +17,29 @@ class DemToCsMap(QDialog):
 
         # ウィンドウタイトル
         self.setWindowTitle("CSMap Plugin")
-        # ラスタデータのみ選択（現状tifのみにしています）
-        self.ui.mQgsFileWidget.setFilter('*.tif')
+        # QGISでサポートされているラスタデータのみ選択可能
+        self.ui.mQgsFileWidget.setFilter('*.tif;;*.tiff;;*.dt0;;*.dt1;;*.dt2;;*.dem;;*.asc;;*.adf;;*.hgt;;*.bil;;*.nc;;*.img;;*.flt;;*.bt;;*.xyz;;*.grd;;*.ter')
         # 出力先をフォルダに指定
         self.ui.mQgsFileWidget_.setStorageMode(1)
 
-        self.ui.pushButton_run.clicked.connect(self.get_and_show_input_text)
+        self.ui.pushButton_run.clicked.connect(self.convert_dem_to_csmap)
         self.ui.pushButton_cancel.clicked.connect(self.close)
 
-    def get_and_show_input_text(self):
+    def convert_dem_to_csmap(self):
         params = process.CsmapParams()
 
         # 入力・出力をUIで操作
         input_path = self.ui.mQgsFileWidget.filePath()
-        output_path = self.ui.mQgsFileWidget_.filePath()
+        output_dir = self.ui.mQgsFileWidget_.filePath()
+
         process.process(
             input_path,
-            output_path=os.path.join(output_path, 'csmap.tif'),
+            output_path=os.path.join(output_dir, 'csmap.tif'),
             chunk_size=256,
             params=params,
         )
 
         # csmap.tifをQGISに読み込む
-        iface.addRasterLayer(os.path.join(output_path, 'csmap.tif'), 'csmap')
+        iface.addRasterLayer(os.path.join(output_dir, 'csmap.tif'), 'csmap')
 
         self.close()
