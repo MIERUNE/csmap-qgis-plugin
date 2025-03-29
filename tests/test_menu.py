af8fef51a33b80dc46ea3f940a7ea7ca2d88984e
KeiTa4446 <geo.jagaimo@gmail.com>
2025-03-29 15:20:54 +0900

    fix

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,5 +1,8 @@
 import os
+import sys
 import unittest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 from processing.core.Processing import Processing
 from qgis.core import QgsProcessingFeedback, QgsProject, QgsRasterLayer
