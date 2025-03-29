7c1575d0403989ae418f5317e24d8537ee5b346e
KeiTa4446 <geo.jagaimo@gmail.com>
2025-03-29 15:33:20 +0900

    fix

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -2,6 +2,10 @@ import os
 import sys
 import unittest
 
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 from processing.core.Processing import Processing
