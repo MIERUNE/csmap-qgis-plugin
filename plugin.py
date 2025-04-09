7eacf2389032f356d2e7185759f8ad60f2c49797
Geo-Jagaimo <geo.jagaimo@gmail.com>
2025-04-09 15:43:40 +0900

    アイコンが無限に生成されるバグを修正

--- plugin.py
+++ plugin.py
@@ -7,7 +7,7 @@ from qgis.core import QgsApplication
 from .processing_provider.csmap_provider import CSMapProcessingProvider
 
 with contextlib.suppress(ImportError):
-    from processing import execAlgorithmDialog
+    from qgis.processing import execAlgorithmDialog
 
 
 class CSMapPlugin:
@@ -19,16 +19,20 @@ class CSMapPlugin:
         QgsApplication.processingRegistry().addProvider(self.provider)
 
         if self.iface:
-            self.setup_algorithms_tool_button()
+            self.setup_algorithm_tool_button()
 
     def unload(self):
+        self.teardown_algorithm_tool_button()
         QgsApplication.processingRegistry().removeProvider(self.provider)
 
-    def setup_algorithms_tool_button(self):
+    def setup_algorithm_tool_button(self):
+        if hasattr(self, "toolButtonAction") and self.toolButtonAction:
+            return
+
         tool_button = QToolButton()
         icon = self.provider.icon()
         default_action = QAction(
-            icon, "CS立体図を作成（CS 3D-Map）", self.iface.mainWindow()
+            icon, self.tr("CS立体図を作成"), self.iface.mainWindow()
         )
         default_action.triggered.connect(
             lambda: execAlgorithmDialog("csmap:dem_to_csmap", {})
@@ -36,3 +40,11 @@ class CSMapPlugin:
         tool_button.setDefaultAction(default_action)
 
         self.toolButtonAction = self.iface.addToolBarWidget(tool_button)
+
+    def teardown_algorithm_tool_button(self):
+        if hasattr(self, "toolButtonAction"):
+            self.iface.removeToolBarIcon(self.toolButtonAction)
+            del self.toolButtonAction
+
+    def tr(self, message):
+        return QgsApplication.translate("CS 3D-Map", message)
